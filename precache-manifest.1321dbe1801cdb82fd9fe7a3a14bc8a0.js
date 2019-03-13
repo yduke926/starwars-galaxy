0d88803500f5b340859ecc5aff3542111035e059
yduke <yduke926@gmail.com>
2019-03-13 01:01:26 -0400

    Updates

---
+++ precache-manifest.1321dbe1801cdb82fd9fe7a3a14bc8a0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c0f27149bdfd30854b24b2b10ffbb6f",
+    "url": "/starwarsgalaxy/static/media/Executor.1c0f2714.jpg"
+  },
+  {
+    "revision": "428d51b7507e20e10fe3",
+    "url": "/starwarsgalaxy/static/css/main.62db6ccc.chunk.css"
+  },
+  {
+    "revision": "7e114fe9763d948fcc27",
+    "url": "/starwarsgalaxy/static/js/runtime~main.ac9df635.js"
+  },
+  {
+    "revision": "574e698c5f254383433fdfbb94d1fc15",
+    "url": "/starwarsgalaxy/static/media/Yavin IV.574e698c.jpg"
+  },
+  {
+    "revision": "c90bc15d8c9b0041d343",
+    "url": "/starwarsgalaxy/static/js/2.288035bf.chunk.js"
+  },
+  {
+    "revision": "4fdbc31f17db3ed618d9abad0e4837f1",
+    "url": "/starwarsgalaxy/static/media/Alderaan.4fdbc31f.jpg"
+  },
+  {
+    "revision": "3b8cdd01a8247ded16023eae17035fe1",
+    "url": "/starwarsgalaxy/static/media/Bespin.3b8cdd01.jpg"
+  },
+  {
+    "revision": "d26ebbef08b256d8134359e71747519c",
+    "url": "/starwarsgalaxy/static/media/Coruscant.d26ebbef.jpg"
+  },
+  {
+    "revision": "4a6be89c3dbfd838919d422a56811b6b",
+    "url": "/starwarsgalaxy/static/media/Dagobah.4a6be89c.jpg"
+  },
+  {
+    "revision": "604eb868737bda4b364f7e794c5f0a73",
+    "url": "/starwarsgalaxy/static/media/Death Star.604eb868.jpg"
+  },
+  {
+    "revision": "7475f119b0749b6c5ac6beea562decd8",
+    "url": "/starwarsgalaxy/static/media/EF76 Nebulon-B escort frigate.7475f119.jpg"
+  },
+  {
+    "revision": "6c79084ffa275b864f276893d68de5f1",
+    "url": "/starwarsgalaxy/static/media/Endor.6c79084f.jpg"
+  },
+  {
+    "revision": "428d51b7507e20e10fe3",
+    "url": "/starwarsgalaxy/static/js/main.23c79fcb.chunk.js"
+  },
+  {
+    "revision": "f5a2c987e9e7b24cf0fef8cd3523c2eb",
+    "url": "/starwarsgalaxy/static/media/Geonosis.f5a2c987.jpg"
+  },
+  {
+    "revision": "a483b49296f4d581523ef6266f9e68e6",
+    "url": "/starwarsgalaxy/static/media/Hoth.a483b492.jpg"
+  },
+  {
+    "revision": "c28b8bd15f963b19d0b9137a1947634b",
+    "url": "/starwarsgalaxy/static/media/Imperial shuttle.c28b8bd1.jpg"
+  },
+  {
+    "revision": "586f2ab18e7ac9bf11a807b2516cd2a7",
+    "url": "/starwarsgalaxy/static/media/Kamino.586f2ab1.jpg"
+  },
+  {
+    "revision": "a010742adccfedbc789a5873a4d63bdf",
+    "url": "/starwarsgalaxy/static/media/Millennium Falcon.a010742a.jpg"
+  },
+  {
+    "revision": "573010fdcda05da83f741134099f4311",
+    "url": "/starwarsgalaxy/static/media/Naboo.573010fd.jpg"
+  },
+  {
+    "revision": "b4e364f375155c693e1c87ca56cec445",
+    "url": "/starwarsgalaxy/static/media/Slave 1.b4e364f3.jpg"
+  },
+  {
+    "revision": "5094236988f170cc60b1deb99d17c383",
+    "url": "/starwarsgalaxy/static/media/TIE Advanced x1.50942369.jpg"
+  },
+  {
+    "revision": "6ce5bcb28cee9229eb91df8ffa0809a4",
+    "url": "/starwarsgalaxy/static/media/X-wing.6ce5bcb2.jpg"
+  },
+  {
+    "revision": "51f430d3d44d2838cfe163b4d4f0f70d",
+    "url": "/starwarsgalaxy/static/media/Y-wing.51f430d3.jpg"
+  },
+  {
+    "revision": "c90bc15d8c9b0041d343",
+    "url": "/starwarsgalaxy/static/css/2.9b5e562a.chunk.css"
+  },
+  {
+    "revision": "11ad121f2ed534b90eb2b613f322f1d7",
+    "url": "/starwarsgalaxy/index.html"
+  }
+];
